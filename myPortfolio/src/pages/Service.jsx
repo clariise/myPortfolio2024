f5c06dfa66384ed5c05cb0a5a46aed3b0ba33801
clariise <clcarmona001@gmail.com>
2024-09-04 18:29:39 +0800

    deleted images

--- myPortfolio/src/pages/Service.jsx
+++ myPortfolio/src/pages/Service.jsx
@@ -4,31 +4,25 @@ function Service() {
       <h1 className="fw-bold">Technology Stack</h1>
       <div className="row justify-content-center">
         <div className="col-md-10 d-flex flex-wrap justify-content-between">
-          <img
-            src="https://w7.pngwing.com/pngs/628/224/png-transparent-bootstrap-plain-wordmark-logo-icon.png"
-            alt="react"
-            height={100}
-            className="m-2"
-          />
+          <img src={img4} alt="react" height={100} className="m-2" />
           <img
             src="https://1000logos.net/wp-content/uploads/2020/08/MySQL-Logo.png  "
             alt="mysql"
             height={100}
             className="m-2"
           />
+          <img src={img3} alt="bootstrap" height={100} className="m-2" />
+
           <img
-            src="https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png"
-            alt="HTML5"
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ad/Figma-1-logo.png"
+            alt="figma"
             height={100}
             className="m-2"
           />
 
-          <img
-            src="https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png"
-            alt="HTML5"
-            height={100}
-            className="m-2"
-          />
+          <img src={img2} alt="firebase" height={100} className="m-2" />
+
+          <img src={img1} alt="laravel" height={100} className="m-2" />
         </div>
       </div>
     </div>
